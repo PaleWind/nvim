de1f1ec9018d747966669c1a789f9a8928f7b472
paul wandrei <paulwandrei@pauls-MacBook-Pro.local>
2023-12-10 17:58:44 -0500

    telescope

--- lua/aoi/keymaps.lua
+++ lua/aoi/keymaps.lua
@@ -7,3 +7,22 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 
+--line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.api.nvim_create_autocmd({"BufEnter", "FocusGained", "InsertLeave", "WinEnter"}, {
+    callback = function()
+        vim.opt.number = true
+    end
+})
+
+vim.api.nvim_create_autocmd({"BufLeave", "FocusLost", "InsertEnter", "WinLeave"}, {
+    callback = function()
+        vim.opt.number = false
+    end
+})
+
+--buffer jumping
+vim.api.nvim_set_keymap('n', '<leader>n', ':bnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>b', ':bprevious<CR>', {noremap = true, silent = true})
+
--- lua/aoi/plugins/colorschemes.lua
+++ lua/aoi/plugins/colorschemes.lua
@@ -1,8 +1,34 @@
 return {
   'rebelot/kanagawa.nvim',
+--  'folke/tokyonight.nvim',
   config = function()
     -- Applying the color scheme
+
     vim.cmd('colorscheme kanagawa')
+--  vim.cmd('colorscheme tokyonight')
+
+    --transparent bg
+    vim.cmd [[
+      hi Normal guibg=NONE ctermbg=NONE
+      hi LineNr guibg=NONE ctermbg=NONE
+      hi Folded guibg=NONE ctermbg=NONE
+      hi NonText guibg=NONE ctermbg=NONE
+      hi SpecialKey guibg=NONE ctermbg=NONE
+      hi VertSplit guibg=NONE ctermbg=NONE
+      hi StatusLine guibg=NONE ctermbg=NONE
+      hi StatusLineNC guibg=NONE ctermbg=NONE
+      hi SignColumn guibg=NONE ctermbg=NONE
+    ]]
+
+    --highlight color
+    vim.api.nvim_set_hl(0, 'Visual', { bg = '#49473e' })
+
+    -- Change the line number color
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = '#ffffff' })
+
+    -- Change the current line number color
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#ffffff' })
+      
   end,
   event = 'VimEnter'  -- or any other suitable event
 }
