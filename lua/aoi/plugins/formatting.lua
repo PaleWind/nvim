b0f01764eda5351affd73eca935893699bf521c3
paul wandrei <paulwandrei@pauls-MacBook-Pro.local>
2023-12-14 00:10:07 -0500

    linting and formatting

---
+++ lua/aoi/plugins/formatting.lua
@@ -0,0 +1,28 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        javascript      = { "prettier" },
+        typescript      = { "prettier" },
+        javascriptreact = { "prettier" },
+        css             = { "prettier" },
+        html            = { "prettier" },
+        json            = { "prettier" },
+        yaml            = { "prettier" },
+        markdown        = { "prettier" },
+        lua             = { "stylua" },
+
+      }
+    })
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500
+      })
+    end, { desc = "Format file" })
+  end
+}
--- lua/aoi/plugins/lsp/lsp_config.lua
+++ lua/aoi/plugins/lsp/lsp_config.lua
@@ -6,11 +6,11 @@ return {
     { "antosha417/nvim-lsp-file-operations", config = true },
   },
   config = function()
-    
+
     local lspconfig = require("lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local keymap = vim.keymap
-    local opts = { noremap = true, silent = true } 
+    local opts = { noremap = true, silent = true }
     local on_attach = function(client, bufnr)
       opts.buffer = bufnr
 
